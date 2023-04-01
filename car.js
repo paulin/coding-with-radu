fd53acf8d39a6592a3b81cde37c1841ab9b4afdc
Matt Paulin <matt@ministryofproduct.com>
2023-04-01 15:29:04 -0700

    neural net installed, car drives randomly

--- car.js
+++ car.js
@@ -14,10 +14,18 @@ class Car{
     this.angle=0;
     this.damaged=false;
 
+    this.useBrain = controlType =="AI";
+
     if(controlType != "DUMMY") {
       this.sensor = new Sensor(this);
+      this.brain=new NeuralNetwork(
+        [this.sensor.rayCount, 6, 4]
+      );
     }
     this.controls = new Controls(controlType);
+
+    this.img=new Image();
+    this.img.src="car.png"
   }
 
   update(roadBorders, traffic) {
@@ -28,6 +36,18 @@ class Car{
     }
     if(this.sensor) {
       this.sensor.update(roadBorders, traffic);
+      const offsets=this.sensor.readings.map(
+          s=>s==null?0:1-s.offset
+      );
+      const outputs=NeuralNetwork.feedForward(offsets,this.brain);
+      console.log(outputs);
+
+      if(this.useBrain) {
+        this.controls.forward=outputs[0];
+        this.controls.left=outputs[1];
+        this.controls.right=outputs[2];
+        this.controls.reverse=outputs[3];
+      }
     }
   }
 
@@ -117,6 +137,8 @@ class Car{
       ctx.lineTo(this.polygon[i].x,this.polygon[i].y);
     }
     ctx.fill();
+
+
     if(this.sensor) {
       this.sensor.draw(ctx);
     }
