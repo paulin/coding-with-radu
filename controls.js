8fe5fd6571c1342182371b017075473db766cf5e
Matt Paulin <matt@ministryofproduct.com>
2023-03-31 19:54:01 -0700

    added a bunch of cars

--- controls.js
+++ controls.js
@@ -1,12 +1,18 @@
 class Controls{
-  constructor(){
+  constructor(type){
     this.forward=false;
     this.left=false;
     this.right=false;
     this.reverse=false;
 
-    this.#addKeyboardListeners();
-
+    switch(type) {
+      case "KEYS":
+        this.#addKeyboardListeners();
+        break;
+      case "DUMMY":
+        this.forward=true;
+        break;
+    }
   }
   #addKeyboardListeners() {
     document.onkeydown=(event)=>{
@@ -41,7 +47,7 @@ class Controls{
           this.reverse=false;
           break;
       }
-      
+
     }
   }
 }
