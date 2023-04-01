cb259e941fa6ef3ada2f557d4391245235ef83af
Matt Paulin <matt@ministryofproduct.com>
2023-04-01 16:38:01 -0700

    lines and nodes added

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const carCanvas=document.getElementById("carCanvas");
 carCanvas.width=200;
+const networkCanvas=document.getElementById("networkCanvas");
+networkCanvas.width=500;
 
 const carCtx = carCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
+
+
 const road=new Road(carCanvas.width/2, carCanvas.width*0.9);
 const car=new Car(road.getLaneCenter(3),100,30,50,"AI","blue");
 const traffic=[
@@ -24,6 +29,8 @@ function animate() {
   car.update(road.borders, traffic);
 
   carCanvas.height=window.innerHeight;
+  networkCanvas.height=window.innerHeight;
+
   carCtx.save();
   carCtx.translate(0,-car.y+carCanvas.height*0.5);
   road.draw(carCtx);
@@ -33,5 +40,6 @@ function animate() {
   car.draw(carCtx);
 
   carCtx.restore();
+  Visualizer.drawNetwork(networkCtx, car.brain);
   requestAnimationFrame(animate);
 }
