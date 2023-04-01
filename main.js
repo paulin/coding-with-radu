f26077187723dcfcbd701da29d6748afa38af743
Matt Paulin <matt@ministryofproduct.com>
2023-04-01 15:41:10 -0700

    refactored in the new canvas

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-const canvas=document.getElementById("myCanvas");
+const carCanvas=document.getElementById("carCanvas");
+carCanvas.width=200;
 
-canvas.width=200;
-
-const ctx = canvas.getContext("2d");
-const road=new Road(canvas.width/2, canvas.width*0.9);
+const carCtx = carCanvas.getContext("2d");
+const road=new Road(carCanvas.width/2, carCanvas.width*0.9);
 const car=new Car(road.getLaneCenter(3),100,30,50,"AI","blue");
 const traffic=[
     new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2,getRandomColor()),
@@ -24,15 +23,15 @@ function animate() {
 
   car.update(road.borders, traffic);
 
-  canvas.height=window.innerHeight;
-  ctx.save();
-  ctx.translate(0,-car.y+canvas.height*0.5);
-  road.draw(ctx);
+  carCanvas.height=window.innerHeight;
+  carCtx.save();
+  carCtx.translate(0,-car.y+carCanvas.height*0.5);
+  road.draw(carCtx);
   for(let i=0; i< traffic.length; i++) {
-    traffic[i].draw(ctx);
+    traffic[i].draw(carCtx);
   }
-  car.draw(ctx);
+  car.draw(carCtx);
 
-  ctx.restore();
+  carCtx.restore();
   requestAnimationFrame(animate);
 }
