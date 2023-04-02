b6731727c99a0614b4e695699d24bd6557fd9c1d
Matt Paulin <matt@ministryofproduct.com>
2023-04-02 07:33:46 -0700

    added buttons to save and reload the best brain

--- main.js
+++ main.js
@@ -14,6 +14,11 @@ const road=new Road(carCanvas.width/2, carCanvas.width*0.9);
 //Generate many cars
 const N = 100;
 const cars=generateCars(N);
+let bestCar = cars[0]; //Use let because it is changing
+if(localStorage.getItem("bestBrain")) {
+  bestCar.brain = JSON.parse(
+    localStorage.getItem("bestBrain"));
+}
 
 const traffic=[
     new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2,getRandomColor()),
@@ -26,6 +31,15 @@ const traffic=[
 ];
 
 animate();
+
+function save() {
+  localStorage.setItem("bestBrain",
+    JSON.stringify(bestCar.brain));
+}
+
+function discard() {
+  localStorage.removeItem("bestBrain");
+}
 
 function generateCars(N) {
   const cars=[];
@@ -46,7 +60,7 @@ function animate(time) {
   }
 
   //syntax to put only the y.s into an array
-  const bestCar=cars.find(
+  bestCar=cars.find(
     c=>c.y==Math.min(
       ...cars.map(c=>c.y)
     ));
